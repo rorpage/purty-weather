fdb2d16e342e1e08319b449f6fafa2a599deb7dd
Tim Miller <tim.miller@e-gineering>
2020-10-17 10:30:32 -0400

    fix up initialization of viewmodels (was using deprecated methods)

--- app/src/main/java/com/rorpage/purtyweather/ui/home/HomeFragment.kt
+++ app/src/main/java/com/rorpage/purtyweather/ui/home/HomeFragment.kt
@@ -6,14 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProviders
 import com.rorpage.purtyweather.R
 
 class HomeFragment : Fragment() {
-    private lateinit var homeViewModel: HomeViewModel
+    private val homeViewModel: HomeViewModel by viewModels()
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView = root.findViewById<TextView>(R.id.text_home)
         homeViewModel.text.observe(viewLifecycleOwner, { s -> textView.text = s })
