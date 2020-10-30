a9381cc24a1e81ee663e5892b7674b295426b036
claytontaylor <taylor.clay@comcast.net>
2020-10-30 16:23:46 -0400

    added feels like to home fragment

--- app/src/main/java/com/rorpage/purtyweather/models/WeatherInfoUnit.kt
+++ app/src/main/java/com/rorpage/purtyweather/models/WeatherInfoUnit.kt
@@ -1,8 +1,10 @@
 package com.rorpage.purtyweather.models
 
+import com.google.gson.annotations.SerializedName
+
 class WeatherInfoUnit : BaseWeatherInfoUnit() {
     var temp = 0.0
 
-    // TODO: 10/9/20 figure out how to make the model name different than the JSON name (feelsLike) [THIS WILL HAPPEN WITH SWITCH TO RETROFIT AND LIKELY MOSHI]
-    var feels_like = 0.0
+    @SerializedName("feels_like")
+    var feelsLike = 0.0
 }
